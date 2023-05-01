4917344f7453378962338666150ebf21c7eb36ce
Lawrencechou19 <lawrencechou19@gmail.com>
2023-05-01 16:08:20 +0800

    2023-05-01

---
+++ W1/W1-04.py
@@ -0,0 +1,24 @@
+def binary_search(list, key):
+    """Returns the position of key in the list if found, -1 otherwise.
+
+    List must be sorted.
+    """
+    left = 0
+    right = len(list) - 1
+    while left <= right:
+        middle = (left + right) // 2
+        
+        if list[middle] == key:
+            return middle
+        if list[middle] > key:
+            right = middle - 1
+        if list[middle] < key:
+            left = middle + 1
+    return -1
+
+print(binary_search("Halloworld","a"))
+
+# wc -l "contact.csv"
+# head -15 contact.csv
+# tail -25 contact.csv
+# test
